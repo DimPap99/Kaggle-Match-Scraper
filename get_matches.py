7935b78c57d9e5df7b909b8819b32ad638a24d81
DimPap99 <dimtrispap1999@gmail.com>
2021-10-21 16:48:32 +0300

    added the script that will get the matches

---
+++ get_matches.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os
+import requests
+import json
+import datetime
+import time
+import glob, sys
+import collections
+from helper import *
+from generate_json import generate
+
